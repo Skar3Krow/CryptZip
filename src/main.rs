490897a72cacdc46158ff4b7f68ed94547934d82
Skar3Krow <dregoexistenciodotofficial@gmail.com>
2024-07-10 01:05:57 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();    
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(ref _stream) => {
                 println!("accepted new connection");
+                handle_client(stream.expect("REASON"));
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    loop {
+        let bytes_read = stream.read(&mut buf).expect("Failed to read from client");
+
+        if bytes_read == 0 {
+            return;
+        }
+
+        stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").expect("Failed to write to client");
     }
 }
