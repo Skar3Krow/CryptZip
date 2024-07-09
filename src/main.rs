5c9060881aa9cfa998ef6d6e9db71ad26c192977
Skar3Krow <dregoexistenciodotofficial@gmail.com>
2024-07-10 02:25:22 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
-
+use std::{io::{Read, Result, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();    
     for stream in listener.incoming() {
         match stream {
-            Ok(ref _stream) => {
+            Ok( _stream) => {
                 println!("accepted new connection");
-                handle_client(stream.expect("Failed to handle client request"));
+                handle_client(_stream).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -16,18 +15,26 @@ fn main() {
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream) -> Result<()>{
     let mut buf = [0; 512];
-    loop {
-        let bytes_read = stream.read(&mut buf).expect("Failed to read from client");
+    stream.read(&mut buf).unwrap();
+    let request=String::from_utf8_lossy(&buf[..]);
+    let lines: Vec<&str> = request.split("\r\n").collect();
+    let tokens: Vec<&str> = lines[0].split(" ").collect();
+    match tokens[0] {
+        "GET" => {
+            if tokens[1] == "/" {
+                stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+            }else if tokens[1].starts_with("/echo/") {
+                let response = tokens[1].replace("/echo/", "");
+                stream.write(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", response.len(), response).as_bytes())?;
+            }else {
+                stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
+            }
+        },
+        _ => println!("Method not Found: {:?}", tokens[0])
 
-        if bytes_read == 0 {
-            return;
-        }
-        if bytes_read > 40 {
-            stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").expect("Failed to write to client");
-        } else {
-            stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").expect("Failed to write to client");
-        }
     }
+    Ok(())
+    
 }
