6e6561f736507870a3b432d2d66f0dcc53bf3614
Skar3Krow <dregoexistenciodotofficial@gmail.com>
2024-07-10 13:36:19 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{Read, Result, Write}, net::{TcpListener, TcpStream}};
+use std::{env, fs, io::{Read, Result, Write}, net::{TcpListener, TcpStream}, thread};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();    
@@ -6,7 +6,7 @@ fn main() {
         match stream {
             Ok( _stream) => {
                 println!("accepted new connection");
-                handle_client(_stream).unwrap();
+                thread::spawn(|| handle_client(_stream).unwrap());
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -25,6 +25,24 @@ fn handle_client(mut stream: TcpStream) -> Result<()>{
         "GET" => {
             if tokens[1] == "/" {
                 stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+            }else if tokens[1].starts_with("/files/"){
+                let filename = tokens[1].replace("/files/", "");
+                let env_args: Vec<_> = env::args().collect();
+                let mut filepath = env_args[2].clone();
+                filepath.push('/');
+                filepath.push_str(&filename);
+                let file = fs::read(&filepath);
+                let mut response = String::from("");
+                match file {
+                    Ok(f) => {
+                        response=format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}", f.len(), String::from_utf8(f).expect("File content"));
+                    },
+                    Err(_) => {
+                        response = format!("HTTP/1.1 404 Not Found\r\n\r\n");
+                        
+                    }
+                }
+                stream.write(response.as_bytes());
             }else if tokens[1].starts_with("/user-agent") {
                 let response = lines[2].replace("User-Agent: ", "");
                 stream.write(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", response.len(), response).as_bytes())?;
